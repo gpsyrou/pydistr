bcff45abfe14cf54c31c694a3516e7b164458871
George Spyrou <gspyrou@umich.edu>
2021-12-24 21:16:29 +0000

    add error handling sample file (to fix later)

---
+++ helpers/ErrorHandling.py
@@ -0,0 +1,6 @@
+
+class NoDataImportedError():
+    """ Class for errors related to data not loaded/not appropriately loaded
+    prior to performing computations (e.g. mean, variance, std, etc).
+    """
+    pass
