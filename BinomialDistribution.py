minor fixes in calculation of sqrt

BinomialDistribution.py
@@ -44,6 +44,6 @@ class BinomialDistribution(ProbabilityDistribution):
             stdev = sqrt(n * p * (1-p))
         """
         variance = self.calculate_variance()
-        self.stdev = np.sqrt(variance)
+        self.stdev = math.sqrt(variance)
 
         return self.stdev

NormalDistribution.py
@@ -47,7 +47,7 @@ class NormalDistribution(ProbabilityDistribution):
         distribution based on the data attribute.
         """
         var = self.calculate_variance(is_sample=is_sample)
-        self.stdev = np.sqrt(var)
+        self.stdev = math.sqrt(var)
 
         return self.stdev
 