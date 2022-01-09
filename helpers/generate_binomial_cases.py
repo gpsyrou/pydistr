a6b3835717e302c936ee44da090e450cc06ad9bd
George Spyrou <gspyrou@umich.edu>
2022-01-09 11:29:06 +0000

    Use Path for output sample cases

--- helpers/generate_binomial_cases.py
+++ helpers/generate_binomial_cases.py
@@ -5,14 +5,14 @@ from pathlib import Path
 PROJECT_PATH = Path(os.path.dirname(__file__)).parent
 output_loc = PROJECT_PATH / Path('input_files')
 
-mu = 0.0
-sigma = 1.0
+num_trials = 10
+prob = 0.5
 num_samples = 1000
 
 np.random.seed(0)
 
-normal_values = np.random.normal(mu, sigma, size=num_samples)
+binomial_values = np.random.binomial(n=num_trials, p=prob, size=num_samples)
 
-with open(os.path.join(output_loc, 'normal_data.txt'), 'w') as f:
-    for item in normal_values:
+with open(os.path.join(output_loc, 'binomial_data.txt'), 'w') as f:
+    for item in binomial_values:
         f.write("%s\n" % item)
